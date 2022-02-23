11a7389dcf1d8c600d2086310d40dba67aa50486
Krirll <balalaev20022@gmail.com>
2022-02-23 18:33:23 +0300

    add di, navigation and osmdroid implementations

--- app/src/main/java/ru/krirll/optimalmaps/presentation/AppActivity.kt
+++ app/src/main/java/ru/krirll/optimalmaps/presentation/AppActivity.kt
@@ -1,9 +1,10 @@
-package ru.krirll.optimalmaps
+package ru.krirll.optimalmaps.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.krirll.optimalmaps.R
 
-class MainActivity : AppCompatActivity() {
+class AppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
