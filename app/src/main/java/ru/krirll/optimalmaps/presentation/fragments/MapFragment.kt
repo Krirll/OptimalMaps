ccbc852e275e74629fd50da24360f4f6443cb54d
Krirll <balalaev20022@gmail.com>
2022-04-24 21:45:18 +0300

    add functions for setting point on map

--- app/src/main/java/ru/krirll/optimalmaps/presentation/fragments/MapFragment.kt
+++ app/src/main/java/ru/krirll/optimalmaps/presentation/fragments/MapFragment.kt
@@ -1,18 +1,32 @@
 package ru.krirll.optimalmaps.presentation.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.preference.PreferenceManager
+import org.osmdroid.api.IGeoPoint
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.CustomZoomButtonsController
+import org.osmdroid.views.overlay.Marker
+import ru.krirll.optimalmaps.R
 import ru.krirll.optimalmaps.databinding.FragmentMapBinding
-import java.lang.RuntimeException
+import ru.krirll.optimalmaps.presentation.viewModels.MapFragmentViewModel
 
 class MapFragment : Fragment() {
+
+    // create/get MapFragmentViewModel in AppActivity context
+    private val mapViewModel by lazy {
+        ViewModelProvider(requireActivity())[MapFragmentViewModel::class.java]
+    }
+
+    private var currentMapCenter: IGeoPoint? = null //for saving center
+    private var currentZoom: Double? = null         //for saving zoom
 
     private var _viewBinding: FragmentMapBinding? = null
     private val viewBinding: FragmentMapBinding
@@ -29,6 +43,35 @@ class MapFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initMap()
+        initSearchButton()
+        observeViewModel()
+    }
+
+    //observe MapFragmentViewModel LiveData
+    private fun observeViewModel() {
+        //observe point
+        mapViewModel.point.observe(viewLifecycleOwner) { point ->
+            //set point on map
+            viewBinding.map.apply {
+            val geoPoint = GeoPoint(point.lat, point.lon)
+                controller.setZoom(point.zoom)
+                setExpectedCenter(geoPoint)
+                val startMarker = Marker(this).apply {
+                    position = geoPoint
+                }
+                overlays.add(startMarker)
+                //refresh MapView
+                invalidate()
+                refreshDrawableState()
+            }
+        }
+    }
+
+    private fun initSearchButton() {
+        viewBinding.searchButton.setOnClickListener {
+            //navigate to SearchFragment
+            findNavController().navigate(R.id.action_mapFragment_to_searchFragment)
+        }
     }
 
     //map settings
@@ -38,9 +81,29 @@ class MapFragment : Fragment() {
             PreferenceManager.getDefaultSharedPreferences(requireContext())
         )
         viewBinding.map.apply {
-            setTileSource(TileSourceFactory.MAPNIK) //standard kind of map
+            //set standard kind of map
+            setTileSource(TileSourceFactory.MAPNIK)
+            //hide zoom buttons
             zoomController.setVisibility(CustomZoomButtonsController.Visibility.NEVER)
-            controller.setZoom(DEFAULT_ZOOM)
+            //access zooming by fingers
+            setMultiTouchControls(true)
+            //set min and max zoom
+            minZoomLevel = MIN_ZOOM
+            maxZoomLevel = MAX_ZOOM
+            //set current zoom
+            controller.setZoom(currentZoom ?: DEFAULT_ZOOM)
+            //set current map center
+            if (currentMapCenter != null) setExpectedCenter(currentMapCenter)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        viewBinding.map.apply {
+            //save current state of map
+            currentMapCenter = mapCenter
+            currentZoom = zoomLevelDouble
+            onPause()
         }
     }
 
@@ -51,7 +114,10 @@ class MapFragment : Fragment() {
     }
 
     companion object {
+
         private const val DEFAULT_ZOOM = 5.0
+        private const val MAX_ZOOM = 20.0
+        private const val MIN_ZOOM = 4.0
     }
 
 }
