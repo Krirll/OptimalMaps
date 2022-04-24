55d9050eee343442f8adb39849182bdb455a9efc
Krirll <balalaev20022@gmail.com>
2022-04-24 21:40:36 +0300

    remove AppACtivity in activity package

--- app/src/main/java/ru/krirll/optimalmaps/presentation/activity/AppActivity.kt
+++ app/src/main/java/ru/krirll/optimalmaps/presentation/activity/AppActivity.kt
@@ -1,79 +1,14 @@
-package ru.krirll.optimalmaps.presentation
+package ru.krirll.optimalmaps.presentation.activity
 
-import android.Manifest
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import ru.krirll.optimalmaps.R
 
 
 class AppActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-    fun checkPermissions() {
-        val permissions: MutableList<String> = ArrayList()
-        var message = "Application permissions:"
-        if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            permissions.add(Manifest.permission.ACCESS_FINE_LOCATION)
-            message += "\nLocation to show user location."
-        }
-        if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-            message += "\nStorage access to store map tiles."
-        }
-        if (!permissions.isEmpty()) {
-            Toast.makeText(this, message, Toast.LENGTH_LONG).show()
-            val params = permissions.toTypedArray()
-            ActivityCompat.requestPermissions(this, params, REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS)
-        } // else: We already have permissions, so handle as normal
-    }
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS -> {
-                val perms: MutableMap<String, Int> = HashMap()
-                // Initial
-                perms[Manifest.permission.WRITE_EXTERNAL_STORAGE] =
-                    PackageManager.PERMISSION_GRANTED
-                // Fill with results
-                var i = 0
-                while (i < permissions.size) {
-                    perms[permissions[i]] = grantResults[i]
-                    i++
-                }
-                // Check for WRITE_EXTERNAL_STORAGE
-                val storage =
-                    perms[Manifest.permission.WRITE_EXTERNAL_STORAGE] == PackageManager.PERMISSION_GRANTED
-                if (!storage) {
-                    // Permission Denied
-                    Toast.makeText(
-                        this,
-                        "Storage permission is required to store map tiles to reduce data usage and for offline usage.",
-                        Toast.LENGTH_LONG
-                    ).show()
-                } // else: permission was granted, yay!
-            }
-        }
-    }
-    companion object {
-
-        private const val REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS = 124
     }
 }
