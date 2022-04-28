307df863a9c3e879c48c6ab5cef9da420494c173
Krirll <balalaev20022@gmail.com>
2022-04-28 23:40:45 +0300

    add location permission check with alert dialogs

--- app/src/main/java/ru/krirll/optimalmaps/presentation/fragments/MapFragment.kt
+++ app/src/main/java/ru/krirll/optimalmaps/presentation/fragments/MapFragment.kt
@@ -1,9 +1,18 @@
 package ru.krirll.optimalmaps.presentation.fragments
 
+import android.app.AlertDialog
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -16,6 +25,7 @@ import org.osmdroid.views.CustomZoomButtonsController
 import org.osmdroid.views.overlay.Marker
 import ru.krirll.optimalmaps.R
 import ru.krirll.optimalmaps.databinding.FragmentMapBinding
+import ru.krirll.optimalmaps.presentation.other.TitleAlertDialog
 import ru.krirll.optimalmaps.presentation.viewModels.MapFragmentViewModel
 
 class MapFragment : Fragment() {
@@ -24,6 +34,18 @@ class MapFragment : Fragment() {
     private val mapViewModel by lazy {
         ViewModelProvider(requireActivity())[MapFragmentViewModel::class.java]
     }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted)
+                initLocationButton()
+            else {
+                createAlertDialogLocationPermissionDenied()
+            }
+        }
+
+    private var alertDialog: AlertDialog? = null
+    private var alertDialogTitle: TitleAlertDialog? = null
 
     private var currentMapCenter: IGeoPoint? = null //for saving center
     private var currentZoom: Double? = null         //for saving zoom
@@ -42,9 +64,105 @@ class MapFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        alertDialogTitle = savedInstanceState?.getParcelable(TITLE)
+        createAlertDialogByTitle(alertDialogTitle)
         initMap()
         initSearchButton()
+        initLocationButton()
         observeViewModel()
+    }
+
+    private fun createAlertDialogByTitle(savedTitle: TitleAlertDialog?) {
+        if (savedTitle != null)
+            when (alertDialogTitle) {
+                TitleAlertDialog.GEO_POSITION_DISABLED -> createAlertDialogGeoPositionDisabled()
+                TitleAlertDialog.LOCATION_PERMISSION_DENIED -> createAlertDialogLocationPermissionDenied()
+                else -> throw RuntimeException("Unknown title $alertDialogTitle")
+            }
+    }
+
+    private fun initLocationButton() {
+        viewBinding.currentLocationButton.setOnClickListener {
+            if (checkSelfPermissionLocation()) {
+                if (isLocationEnabled()) {
+                    //update current user location on map  and change location button icon
+                } else
+                    createAlertDialogGeoPositionDisabled()
+            } else
+                checkPermission()
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager = requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                && locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    //check location permission
+    private fun checkPermission() {
+        when {
+            //if granted
+             checkSelfPermissionLocation() -> {
+                //update current user location on map and change location button icon
+            }
+            //if was denied and should show rationale
+            shouldShowRequestPermissionRationale(android.Manifest.permission.ACCESS_FINE_LOCATION) -> {
+                createAlertDialogLocationPermissionDenied()
+            }
+            //if was denied
+            else ->
+                requestPermissionLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+    private fun checkSelfPermissionLocation() =
+        ContextCompat.checkSelfPermission(
+            requireContext(),
+            android.Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+
+    private fun createAlertDialogLocationPermissionDenied() {
+        createAlertDialog(
+            TitleAlertDialog.LOCATION_PERMISSION_DENIED,
+            getString(R.string.location_rationale)
+        ) {
+            startActivity(
+                Intent(
+                    Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                    Uri.fromParts("package", requireActivity().packageName, null)
+                )
+            )
+        }
+    }
+
+    private fun createAlertDialogGeoPositionDisabled() {
+        createAlertDialog(
+            TitleAlertDialog.GEO_POSITION_DISABLED,
+            getString(R.string.location_disabled)
+        ) {
+            startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+        }
+    }
+
+    private fun createAlertDialog(
+        title: TitleAlertDialog,
+        message: String,
+        positiveFunction: () -> Unit
+    ) {
+        alertDialog =
+            AlertDialog.Builder(requireContext())
+                .setMessage(message)
+                .setTitle(title.stringRes)
+                .setCancelable(false)
+                .setPositiveButton(R.string.ok) { dialog, _ ->
+                    dialog.dismiss()
+                    positiveFunction()
+                }
+                .setNegativeButton(R.string.cancel) { dialog, _ -> dialog.dismiss() }
+                .create()
+        alertDialog?.show()
+        alertDialogTitle = title
     }
 
     //observe MapFragmentViewModel LiveData
@@ -53,11 +171,12 @@ class MapFragment : Fragment() {
         mapViewModel.point.observe(viewLifecycleOwner) { point ->
             //set point on map
             viewBinding.map.apply {
-            val geoPoint = GeoPoint(point.lat, point.lon)
+                val geoPoint = GeoPoint(point.lat, point.lon)
                 controller.setZoom(point.zoom)
-                setExpectedCenter(geoPoint)
+                controller.animateTo(geoPoint)
                 val startMarker = Marker(this).apply {
                     position = geoPoint
+                    //TODO Дополнить информацией диалоговое окно
                 }
                 overlays.add(startMarker)
                 //refresh MapView
@@ -105,6 +224,7 @@ class MapFragment : Fragment() {
             currentZoom = zoomLevelDouble
             onPause()
         }
+        alertDialog?.dismiss()
     }
 
     //clean view binding
@@ -113,11 +233,17 @@ class MapFragment : Fragment() {
         _viewBinding = null
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putParcelable(TITLE, alertDialogTitle) //save title of current alert dialog
+        super.onSaveInstanceState(outState)
+    }
+
     companion object {
 
         private const val DEFAULT_ZOOM = 5.0
         private const val MAX_ZOOM = 20.0
         private const val MIN_ZOOM = 4.0
+        private const val TITLE = "TITLE"
     }
 
 }
