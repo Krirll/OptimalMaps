6d0195e666b40ebee73a2e13ca7ff78fce976b63
Krirll <balalaev20022@gmail.com>
2022-05-10 13:07:10 +0300

    edit live data (value is nullable)

--- app/src/main/java/ru/krirll/optimalmaps/presentation/viewModels/MapFragmentViewModel.kt
+++ app/src/main/java/ru/krirll/optimalmaps/presentation/viewModels/MapFragmentViewModel.kt
@@ -1,17 +1,20 @@
 package ru.krirll.optimalmaps.presentation.viewModels
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ru.krirll.optimalmaps.domain.model.PointItem
 
 class MapFragmentViewModel : ViewModel() {
 
-    private var _point = MutableLiveData<PointItem>()
-    val point : LiveData<PointItem>
+    private var _point = MutableLiveData<PointItem?>()
+    val point : MutableLiveData<PointItem?>
         get() = _point
 
-    fun setMapPoint(point : PointItem) {
+    fun setPoint(point : PointItem) {
         _point.value = point
+    }
+
+    fun removePoint() {
+        _point.postValue(null)
     }
 }
