d36b5cd1bb0e51dd2baa967fb7e9ecb52a0bec5c
Krirll <balalaev20022@gmail.com>
2022-05-27 13:14:58 +0300

    update

--- app/src/main/java/ru/krirll/optimalmaps/domain/repository/PointRepository.kt
+++ app/src/main/java/ru/krirll/optimalmaps/domain/repository/PointRepository.kt
@@ -3,6 +3,8 @@ package ru.krirll.optimalmaps.domain.repository
 import androidx.lifecycle.LiveData
 import org.osmdroid.bonuspack.routing.Road
 import ru.krirll.optimalmaps.domain.model.PointItem
+import ru.krirll.optimalmaps.domain.model.RouteItem
+import ru.krirll.optimalmaps.presentation.enums.RouteError
 
 interface PointRepository {
 
@@ -20,7 +22,11 @@ interface PointRepository {
     suspend fun createRoute(
         points: List<PointItem>,
         withEndPoint: Boolean,
-        onErrorEventListener: (Int) -> Unit
+        onErrorEventListener: (RouteError) -> Unit
     ): Road?
+
+    fun loadRouteHistory(): LiveData<List<RouteItem>>
+
+    suspend fun saveRoute(route: Road, points: String, list: List<PointItem>)
 
 }
